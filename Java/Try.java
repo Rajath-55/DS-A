f6fa9ad4a2ac5b96944034ec68d06b5703ef46df
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2020-09-05 13:10:04 +0530

    FIRST

---
+++ Java/Try.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+class Try {
+
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+
+    
+    }
+}
