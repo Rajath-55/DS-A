79eb09f4acd87fff983794dd808f9af217e62bad
Rajath V <rajathv@localhost.localdomain>
2021-02-14 18:52:15 +0530

    test

--- C++/new.cpp
+++ C++/new.cpp
@@ -1,62 +1,18 @@
 #include <bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-// template <typename T>
-// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-vector<vector<int>> threeSum(vector<int> &nums)
-{
-    vector<vector<int>> res;
-    if (nums.size() == 0 || nums.size() == 1 || nums.size() == 2)
-        return {};
-    sort(nums.begin(), nums.end());
-    set<vector<int>> s;
-    for (int i = 0; i < nums.size() - 2; ++i)
-    {
-        int j = i + 1, k = nums.size() - 1;
-        while (j < k)
-        {
-            int su = nums[i] + nums[j] + nums[k];
-            cout<<nums[i]<<" "<<nums[j]<<" "<<nums[k]<<endl;
-            cout<<su<<endl;
-            if (su == 0){
-                s.insert({nums[i], nums[j], nums[k]});
-                j++;
-            }
-            if (su > 0)
-                k--;
-            if (su < 0)
-                j++;
-        }
-    }
-    for (auto x : s)
-        res.push_back(x);
-    return res;
-}
-
+#define mod (int)(1e9+7);
 int main()
 {
-    vector<int>temp={-2,0,1,1,2};
-    vector<vector<int>>res = threeSum(temp);
-    for(auto x : res)
-    {
-        for(auto y : x){
-            cout<<y<<" ";
+
+    int n;
+    cin >> n;
+    vector<vector<int>>coins(n, vector<int>(n,0));
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cin>>coins[i][j];
         }
-        cout<<endl;
     }
     
-}
 
-/*
-555
-550
-505
-500
-055
-050
-005
-000
-*/
+    return 0;
+}
--- C/trial.c
+++ C/trial.c
@@ -1,28 +1,118 @@
 #include <stdio.h>
+int n;
+struct date
+{
+    int dd;
+    int mm;
+    int yy;
+};
+struct tme
+{
+    int hour;
+    int min;
+    int sec;
+};
+struct timestamp
+{
+    struct date dt;
+    struct tme tme;
+};
 
-void swap(char a, char b){
-    char temp = a;
-    a = b;
-    b = temp;
+void Sort_Tokens(int n, struct timestamp arr[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i].dt.yy > arr[j].dt.yy)
+            {
+                struct timestamp p;
+                p = arr[i];
+                arr[i] = arr[j];
+                arr[j] = p;
+                continue;
+            }
+            if (arr[i].dt.yy == arr[j].dt.yy)
+            {
+                if (arr[i].dt.mm > arr[j].dt.mm)
+                {
+                    struct timestamp p;
+                    p = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = p;
+                    continue;
+                }
+                if (arr[i].dt.mm == arr[j].dt.mm)
+                {
+                    if (arr[i].dt.dd > arr[j].dt.dd)
+                    {
+                        struct timestamp p;
+                        p = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = p;
+                        continue;
+                    }
+                    if (arr[i].dt.dd == arr[j].dt.dd)
+                    {
+                        if (arr[i].tme.hour > arr[j].tme.hour)
+                        {
+                            struct timestamp p;
+                            p = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = p;
+                            continue;
+                        }
+                        if (arr[i].tme.hour == arr[j].tme.hour)
+                        {
+                            if (arr[i].tme.min > arr[j].tme.min)
+                            {
+                                struct timestamp p;
+                                p = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = p;
+                                continue;
+                            }
+                            if (arr[i].tme.min == arr[j].tme.min)
+                            {
+                                if (arr[i].tme.sec > arr[j].tme.sec)
+                                {
+                                    struct timestamp p;
+                                    p = arr[i];
+                                    arr[i] = arr[j];
+                                    arr[j] = p;
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
- 
-char *revs(char *s, int start, int end){
-     if ((s[start] >= 'd' || s[start] >= 'D') && (s[start] <= 's' || s[start] <= 'S'))
-     {
-         if ((s[end] >= 'd' || s[end] >= 'D') && (s[end] <= 's' || s[end] <= 'S'))
-         {
-             swap(s[start], s[end]);
-         }
-     }
-     if (end - start <= 2)
-     {
-         return s;
-     }
 
-     return revs(s, start + 1, end - 1);
- }
+void read(struct timestamp *arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d %d %d %d %d", &arr[i].dt.dd, &arr[i].dt.mm, &arr[i].dt.yy, &arr[i].tme.hour, &arr[i].tme.min, &arr[i].tme.sec);
+    }
+}
+void print(struct timestamp *arr)
+{
+    printf("%d\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %d %d %d %d %d\n", arr[i].dt.dd, arr[i].dt.mm, arr[i].dt.yy, arr[i].tme.hour, arr[i].tme.min, arr[i].tme.sec);
+    }
+}
 
-int main(){
-
-   printf("%s ", revs("Devansh", 0, 6));
+int main()
+{
+    scanf("%d", &n);
+    struct timestamp arr[n];
+    read(arr);
+    Sort_Tokens(n, arr);
+    print(arr);
+    return 0;
 }
